8052efdc1763ad1f2dacdf77a7bf3e25039af805
Kat <57116477+kmuhin@users.noreply.github.com>
2022-07-10 14:54:22 +0300

    fixes catching errors removed unicode chcp 65001

--- auto.cmd
+++ auto.cmd
@@ -4,6 +4,11 @@ set history_dates=history_dates.txt
 set dashcam_drive=D:
 set videos_dir=videos
 set cmd_ffmpeg=ffmpeg
+set cmd_concat=auto_2_concat.cmd
+set cmd_convert=auto_3_convert.cmd
+:: История обработанных дат на время выполнения скрипта
+set history_tmp=history_dates.tmp
+
 
 if not exist "%videos_dir%" (
     mkdir "%videos_dir%"
@@ -15,6 +20,7 @@ SETLOCAL EnableDelayedExpansion
 if not exist "%history_dates%" (
     type NUL > "%history_dates%"
 )
+del "%history_tmp%"
 
 for /F "tokens=*" %%i in ('dir /b /o:N D:\Normal') do (
     set tmpvar=%%i
@@ -32,8 +38,14 @@ for /F "tokens=*" %%i in ('dir /b /o:N D:\Event') do (
 goto end
 
 :maketmpincludes
-findstr %1 "%history_dates%" >nul 2>&1
-if %ERRORLEVEL% NEQ 0 (
+    :: Дата есть в списке обработанных
+    findstr %1 "%history_dates%" >nul 2>&1
+    if %ERRORLEVEL% EQU 0 EXIT /B
+    :: Дата есть во временном списке обработанных, но, вероятно, что-то пошло не так.
+    findstr %1 "%history_tmp%" >nul 2>&1
+    if %ERRORLEVEL% EQU 0 EXIT /B
+    :: Сохраняю дату во временный список
+    echo !tmpdate! >> "%history_tmp%"
     echo new video date: %1
     set dt=%1
     set DAY=!dt:~6,2!
@@ -44,11 +56,13 @@ if %ERRORLEVEL% NEQ 0 (
     set filename_speed=%videos_dir%\!filemaskdate!_x8.mkv
     echo filename_concat: !filename_concat!
     echo filename_speed: !filename_speed!
-    call 2_concat.cmd
-    call 3_convert.cmd
+    call "%cmd_concat%"
+    if %ERRORLEVEL% NEQ 0 EXIT /B 1
+    call "%cmd_convert%"
+    if %ERRORLEVEL% NEQ 0 EXIT /B 1
+    :: Ошибок нет. Сохраняю дату во список обработанных
     echo !tmpdate! >> "%history_dates%"
-)
-:: возвращаюсь к циклу
-EXIT /B
+    :: возвращаюсь к циклу
+    EXIT /B
 
 :end
--- auto_2_concat.cmd
+++ auto_2_concat.cmd
@@ -1,56 +1,55 @@
 @echo off
-chcp 65001
-rem Скрипт запускается из другого скрипта, из которого берутся переменные
-rem %YEAR%, %MONTH, %%DAY% - год, месяц, день
-rem %filename_concat% - файл вывода, объединенные в один файл видеозаписи с SD
-rem %dashcam_drive% - диск с файлами
-rem %cmd_ffmpeg% - команда запуска ffmpeg
+:: Скрипт запускается из другого скрипта, из которого берутся переменные
+:: %YEAR%, %MONTH, %%DAY% - год, месяц, день
+:: %filename_concat% - файл вывода, объединенные в один файл видеозаписи с SD
+:: %dashcam_drive% - диск с файлами
+:: %cmd_ffmpeg% - команда запуска ffmpeg
 
-rem == Set high-performance power scheme to speed job ==
-rem call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+:: == Set high-performance power scheme to speed job ==
+:: call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 set file_mask=%YEAR%%MONTH%%DAY%-*
 set file_mask_normal=FILE%file_mask%
 set file_mask_event=EMER%file_mask%
-rem Штатные видеозаписи
+:: Штатные видеозаписи
 set dir_normal=%dashcam_drive%\Normal
-rem Особые видеозаписи (сработал датчик удара)
+:: Особые видеозаписи (сработал датчик удара)
 set dir_event=%dashcam_drive%\Event
 set tmp=%~dpn0%.tmp
 set tmp_normal=%~dpn0%_normal.tmp
 set tmp_event=%~dpn0%_event.tmp
 set tmp_all=%~dpn0%_all.tmp
 
-rem type NUL > %tmp%
+:: type NUL > %tmp%
 del "%tmp%"
 type NUL > "%tmp_normal%"
 type NUL > "%tmp_event%"
 type NUL > "%tmp_all%"
 
-rem Штатные видеозаписи. Сохраняю два значения разделенных ";": только имя файла;полный путь файла.
-rem Позже использую sort /+5 для сортировки начиная с даты (5-ый символ). В данном случае префиксы в именах файлов одинаковой длины.
-rem Для префиксов разной длины можно пойти другим путем. Сразу вырезать из имени файла символы до даты, к примеру, удаляю первые 4 символа: 
-rem  SETLOCAL EnableDelayedExpansion
-rem  for /F "tokens=*" %%i in ('dir /b %dir_normal%\%file_mask_normal%') do (
-rem     set tmpvar=%%i
-rem     echo !tmpvar:~4!;%dir_normal%\%%i>> %tmp_normal%
-rem   )
-rem Далее sort использовать без  параметров.
+:: Штатные видеозаписи. Сохраняю два значения разделенных ";": только имя файла;полный путь файла.
+:: Позже использую sort /+5 для сортировки начиная с даты (5-ый символ). В данном случае префиксы в именах файлов одинаковой длины.
+:: Для префиксов разной длины можно пойти другим путем. Сразу вырезать из имени файла символы до даты, к примеру, удаляю первые 4 символа: 
+::  SETLOCAL EnableDelayedExpansion
+::  for /F "tokens=*" %%i in ('dir /b %dir_normal%\%file_mask_normal%') do (
+::     set tmpvar=%%i
+::     echo !tmpvar:~4!;%dir_normal%\%%i>> %tmp_normal%
+::   )
+:: Далее sort использовать без  параметров.
 
-for /F "tokens=*" %%i in ('dir /b %dir_normal%\%file_mask_normal%') do (
+for /F "tokens=*" %%i in ('dir /b "%dir_normal%\%file_mask_normal%"') do (
 	echo %%i;%dir_normal%\%%i>> %tmp_normal%
 )
 
-rem Особые видеозаписи (сработал датчик удара)
-for /F "tokens=*" %%i in ('dir /b %dir_event%\%file_mask_event%') do (
+:: Особые видеозаписи (сработал датчик удара)
+for /F "tokens=*" %%i in ('dir /b "%dir_event%\%file_mask_event%"') do (
 	echo %%i;%dir_event%\%%i>> %tmp_event%
 )
 
-rem Объединяю файлы и сортирую результат начиная с 5-го символа
-rem т.е. фактически сортирую по датам в именах файлов.
+:: Объединяю файлы и сортирую результат начиная с 5-го символа
+:: т.е. фактически сортирую по датам в именах файлов.
 type %tmp_normal% %tmp_event% 2>nul | sort /+5 > %tmp_all%
 
-rem финальный файл для ffmpeg
+:: финальный файл для ffmpeg
 for /F "tokens=1,2 delims=;" %%i in (%tmp_all%) do (
     echo file '%%j'>> %tmp%
     echo %%i
@@ -63,7 +62,7 @@ if not exist "%tmp%" (
     goto END
 )
 
-rem Запускаю слияние файлов
+:: Запускаю слияние файлов
 "%cmd_ffmpeg%" -f concat -safe 0 -i %tmp% -c copy "%filename_concat%"
 echo  
 
